29c0cb8c552d4e8bbdf3f4f444efd783b4d0ece5
Andy Madjedi <13892787+andymdj@users.noreply.github.com>
2025-03-09 18:09:12 -0700

    Create and destroy philosopher threads

--- philosophers.c
+++ philosophers.c
@@ -1,6 +1,23 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void* philosopher(void* id) {
+    printf("Philosopher %d\n", *(int*)id);
+}
 
 int main() {
+    // Create philosopher threads passing each an id.
+    pthread_t thread_ids[5];
+    for(int i = 0; i < 5; i++) {
+        int* idPtr = malloc(sizeof(*idPtr));
+        *idPtr = i;
+        pthread_create(&thread_ids[i], NULL, &philosopher, idPtr);
+    }
+
+    // Close all philosopher threads.
+    for(int i = 0; i < 5; i++) {
+        pthread_join(thread_ids[i], NULL);
+    }
     return 0;
 }
